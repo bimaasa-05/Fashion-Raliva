adbd36145371e60fc2aa0db9fd4d2126465762a5
Lorantz07 <gilasihhh0@gmail.com>
2026-08-24 09:21:38 +0700

    Menambahkan mock.customer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->alias([
+            'mock.customer' => \App\Http\Middleware\EnsureMockCustomer::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
