41ad2e95da1e66bde6870aef4a5c99193826999f
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-28 19:07:04 +0700

    Memperbaiki Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             SuperAdminSeeder::class,
             UserSeeder::class,
+            WalletSeeder::class,
         ]);
     }
 }
