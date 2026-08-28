dff6a18b5bff8092c5e0c956f6d550333000ac42
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-28 18:36:32 +0700

    Memperbaiki PengajuanTokoController di Owner dan memperbaiki logic di bagian backendnya juga

--- app/Http/Controllers/Owner/PengajuanTokoController.php
+++ app/Http/Controllers/Owner/PengajuanTokoController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Support\OwnerContext;
+use Illuminate\Http\Request;
 
 class PengajuanTokoController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('Owner.pengajuan-toko.index');
+        $store = OwnerContext::currentStore();
+
+        return view('Owner.pengajuan-toko.index', compact('store'));
     }
 }
-
