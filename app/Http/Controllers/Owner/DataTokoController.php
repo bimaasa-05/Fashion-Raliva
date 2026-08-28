f362f42eaeb992798f38d5c46c1ba249a200835b
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-28 18:28:49 +0700

    Memperbaiki Owner pada DataTokoController di Bagian logicnya

--- app/Http/Controllers/Owner/DataTokoController.php
+++ app/Http/Controllers/Owner/DataTokoController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Support\OwnerContext;
+use App\Models\Review;
 
 class DataTokoController extends Controller
 {
     public function index()
     {
-        return view('Owner.data-toko.index');
+        $store = OwnerContext::currentStore();
+        $rating = $store ? (float) Review::where('store_id', $store->store_id)->avg('rating') : 0;
+        $reviewCount = $store ? Review::where('store_id', $store->store_id)->count() : 0;
+
+        return view('Owner.data-toko.index', compact('store', 'rating', 'reviewCount'));
     }
 }
-
