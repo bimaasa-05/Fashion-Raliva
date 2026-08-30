d8a6a6ac8665ecbe417caef41119bf2fa495ce93
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-08-30 21:47:39 +0700

    Memperbaiki Backend pada RiwayatAktivitasController terutama di bagain Logicnya juga:

--- app/Http/Controllers/Admin/RiwayatAktivitasController.php
+++ app/Http/Controllers/Admin/RiwayatAktivitasController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ActivityLog;
+use Illuminate\Http\Request;
 
 class RiwayatAktivitasController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('Admin.riwayat-aktivitas.index');
+        $logs = ActivityLog::with('user')
+            ->orderByDesc('created_at')
+            ->paginate(10);
+
+        return view('Admin.riwayat-aktivitas.index', compact('logs'));
     }
 }
