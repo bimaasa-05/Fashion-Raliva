4358fcb377debdde565d43fc02f2f6d866b3d53a
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-04 20:15:23 +0700

    Memperbaiki pada Logic stoknya

--- app/Http/Controllers/Admin/StokController.php
+++ app/Http/Controllers/Admin/StokController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\WarehouseStock;
+use App\Support\AdminContext;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
@@ -11,7 +12,9 @@ class StokController extends Controller
 {
     public function index()
     {
+        $storeIds = AdminContext::assignedStoreIds();
         $stocks = WarehouseStock::with(['productVariant.product', 'warehouse'])
+            ->whereHas('warehouse', fn($q) => $q->whereIn('store_id', $storeIds))
             ->orderBy('warehouse_stock_id')
             ->paginate(15);
 
@@ -20,6 +23,11 @@ class StokController extends Controller
 
     public function update(Request $request, WarehouseStock $warehouseStock): RedirectResponse
     {
+        $storeIds = AdminContext::assignedStoreIds();
+        if (! in_array($warehouseStock->warehouse->store_id, $storeIds)) {
+            abort(403);
+        }
+
         $data = $request->validate([
             'jumlah_stok' => 'required|integer|min:0',
         ]);
