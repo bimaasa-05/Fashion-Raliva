1b1dabba292a93a513447c444ae2d0b8f7b51e7a
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-04 20:02:31 +0700

    Memperbaiki logic pada riwayat aktivitasnya

--- app/Http/Controllers/Admin/RiwayatAktivitasController.php
+++ app/Http/Controllers/Admin/RiwayatAktivitasController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\ActivityLog;
+use App\Models\StoreStaff;
+use App\Support\AdminContext;
 use Illuminate\Http\Request;
 
 class RiwayatAktivitasController extends Controller
 {
     public function index(Request $request)
     {
+        $storeIds = AdminContext::assignedStoreIds();
+        $userIds = StoreStaff::whereIn('store_id', $storeIds)->pluck('user_id')->push(auth()->id())->unique()->filter();
+
         $logs = ActivityLog::with('user')
+            ->whereIn('user_id', $userIds)
             ->orderByDesc('created_at')
             ->paginate(10);
 
