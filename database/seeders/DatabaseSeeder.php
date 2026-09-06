a61c70ccfd39e9bac79a00318e2a519bf12b5e38
Lorantz07 <gilasihhh0@gmail.com>
2026-09-06 07:48:38 +0700

    Menambahkan RiviewSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             SuperAdminSeeder::class,
             UserSeeder::class,
             ProductSeeder::class,
+            ReviewSeeder::class,
         ]);
     }
 }
