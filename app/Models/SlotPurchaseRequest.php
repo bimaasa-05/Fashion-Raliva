19789b8bd2ced83ba8f30985d126c6c6e5c27ace
dulsxcod <xiaomijatibarang40@gmail.com>
2026-09-08 11:22:43 +0700

    menambahkan harga per 1 slot, total harga, payment status, metode pembayaran, dan biaya layanan

--- app/Models/SlotPurchaseRequest.php
+++ app/Models/SlotPurchaseRequest.php
@@ -15,21 +15,36 @@ class SlotPurchaseRequest extends Model
 
     public const STATUS_DITOLAK = 'ditolak';
 
+    public const PEMBAYARAN_MENUNGGU_BAYAR = 'menunggu_bayar';
+
+    public const PEMBAYARAN_MENUNGGU_VERIFIKASI = 'menunggu_verifikasi';
+
+    public const PEMBAYARAN_TERVERIFIKASI = 'terverifikasi';
+
+    public const PEMBAYARAN_DITOLAK = 'ditolak';
+
     protected $fillable = [
         'store_id',
         'jumlah_slot',
+        'harga_per_slot',
+        'total_harga',
+        'payment_status',
+        'metode_pembayaran',
+        'biaya_layanan',
         'alasan',
         'file_bukti',
         'status',
         'alasan_penolakan',
         'handled_by',
         'diajukan_pada',
+        'paid_at',
     ];
 
     protected function casts(): array
     {
         return [
             'diajukan_pada' => 'datetime',
+            'paid_at' => 'datetime',
         ];
     }
 
