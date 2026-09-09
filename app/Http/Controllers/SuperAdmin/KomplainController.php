56b00059dc5a541349be00c86ee0d0b52330d3e5
dulsxcod <xiaomijatibarang40@gmail.com>
2026-09-09 12:17:42 +0700

    mengubah status ditutup mejadi status selesai

--- app/Http/Controllers/SuperAdmin/KomplainController.php
+++ app/Http/Controllers/SuperAdmin/KomplainController.php
@@ -160,7 +160,7 @@ class KomplainController extends Controller
 
     public function tutup(Request $request, Complaint $komplain)
     {
-        if (! in_array($komplain->status, [Complaint::STATUS_OPEN, Complaint::STATUS_DIPROSES], true)) {
+        if (! in_array($komplain->status, [Complaint::STATUS_OPEN, Complaint::STATUS_DIPROSES, Complaint::STATUS_ESKALASI, 'baru'], true)) {
             return back()->with('toast', [
                 'message' => 'Komplain ini sudah ditutup atau selesai.',
                 'icon' => 'gpp_maybe',
@@ -174,7 +174,7 @@ class KomplainController extends Controller
         $lama = $komplain->only(['status']);
 
         $komplain->update([
-            'status' => Complaint::STATUS_DITUTUP,
+            'status' => Complaint::STATUS_SELESAI,
             'diselesaikan_pada' => now(),
         ]);
 
@@ -198,7 +198,7 @@ class KomplainController extends Controller
             Complaint::class,
             $komplain->complaint_id,
             $lama,
-            ['status' => Complaint::STATUS_DITUTUP],
+            ['status' => Complaint::STATUS_SELESAI],
             sprintf('Menutup komplain "%s".', $komplain->subjek)
         );
 
