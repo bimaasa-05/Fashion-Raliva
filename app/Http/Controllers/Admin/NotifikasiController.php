c635243c5986d93cceb3e39f758c62f6f2b20286
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-09 08:38:49 +0700

    Menambahkan controller baru untuk si admin dan biar muncul la notifkasinya jir

---
+++ app/Http/Controllers/Admin/NotifikasiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\NotifikasiController as RootNotifikasiController;
+use App\Models\Notification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class NotifikasiController extends RootNotifikasiController
+{
+    public function index(Request $request)
+    {
+        $notifications = Notification::with('aktor:user_id,nama_lengkap,foto_profil')
+            ->forUser(Auth::id())
+            ->latest()
+            ->paginate(15)
+            ->withQueryString();
+
+        return view('Admin.notifikasi.index', [
+            'notifications' => $notifications,
+        ]);
+    }
+}
