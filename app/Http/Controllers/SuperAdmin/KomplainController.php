3a9dbc18f24907b6e09437f7ecfeb9b53ac8ee19
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-12 09:03:12 +0700

    Memperbaiki beberapa bug

--- app/Http/Controllers/SuperAdmin/KomplainController.php
+++ app/Http/Controllers/SuperAdmin/KomplainController.php
@@ -145,7 +145,7 @@ class KomplainController extends Controller
 
         $lama = $komplain->only(['status']);
 
-        $komplain->update(['status' => Complaint::STATUS_DIPROSES]);
+        $komplain->update(['status' => Complaint::STATUS_ESKALASI]);
 
         $actorId = ActivityLogger::resolveActorId();
 
@@ -173,7 +173,7 @@ class KomplainController extends Controller
             Complaint::class,
             $komplain->complaint_id,
             $lama,
-            ['status' => Complaint::STATUS_DIPROSES],
+            ['status' => Complaint::STATUS_ESKALASI],
             sprintf('Mengeskalasi komplain "%s" ke Owner toko %s.', $komplain->subjek, $komplain->store->nama_toko ?? '-')
         );
 
