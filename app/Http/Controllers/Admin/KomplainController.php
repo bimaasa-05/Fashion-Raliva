bef7e9ce226c35fbb4b3e5c603d17c7e59f70a8e
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-12 13:28:33 +0700

    Memperbaiki logic pada KomplainController dan menambahkan beberapa tampilan untuk detailnya juga

--- app/Http/Controllers/Admin/KomplainController.php
+++ app/Http/Controllers/Admin/KomplainController.php
@@ -17,7 +17,7 @@ class KomplainController extends Controller
     public function index()
     {
         $storeIds = AdminContext::assignedStoreIds();
-        $complaints = Complaint::with(['user', 'order', 'messages.sender'])
+        $complaints = Complaint::with(['user', 'order.store', 'orderItem.productVariant.product', 'store', 'messages.sender'])
             ->whereHas('order', fn($q) => $q->whereIn('store_id', $storeIds))
             ->orderByRaw("CASE status WHEN 'open' THEN 0 WHEN 'diproses' THEN 1 WHEN 'escalated' THEN 2 ELSE 3 END")
             ->orderByDesc('dibuat_pada')
