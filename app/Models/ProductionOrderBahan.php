a52093d595c0b8504bc1af54ef212b4306ed0294
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-21 15:50:30 +0700

    Menambahkan beberapa models baru untuk production order

--- app/Models/ProductionOrderBahan.php
+++ app/Models/ProductionOrderBahan.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class ProductionOrderBahan extends Model
 {
     protected $table = 'production_order_bahan';
+
+    public const SUMBER_ADMIN = 'admin';
+
+    public const SUMBER_PRODUKSI = 'produksi';
+
+    public const SATUAN = ['meter', 'cm', 'yard', 'roll', 'kg', 'gram', 'pcs'];
+
     protected $fillable = [
         'order_id',
         'bahan_id',
@@ -15,8 +22,16 @@ class ProductionOrderBahan extends Model
         'jumlah',
         'satuan',
         'catatan',
+        'sumber',
+        'dibuat_oleh_role',
         'created_by',
     ];
+
+    public function isDariProduksi(): bool
+    {
+        return $this->sumber === self::SUMBER_PRODUKSI
+            || $this->dibuat_oleh_role === 'Produksi';
+    }
 
     public function order(): BelongsTo
     {
