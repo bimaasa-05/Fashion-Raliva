71cbf6a092044377bdeae564361537ba9eb49d2b
dulsxcod <xiaomijatibarang40@gmail.com>
2026-09-24 15:35:41 +0700

    memperbaiki bug waktu jam pada tangguhkan toko

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \App\Http\Middleware\ReactivateExpiredStores::class,
         ]);
         $middleware->alias([
             'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
