097781ca4595a56f7855e894fc8bd6fcdf70f3be
bimaasa-05 <callmexmeliodas1@gmail.com>
2026-09-24 21:39:58 +0700

    Menambahkan beberapa product di calculation kan kayaknya

--- tests/Unit/ProductCostCalculatorTest.php
+++ tests/Unit/ProductCostCalculatorTest.php
@@ -20,6 +20,7 @@ class ProductCostCalculatorTest extends TestCase
         );
 
         $this->assertSame(13000.0, $result['modal_bahan']);
+        $this->assertSame(2000.0, $result['biaya_operasional']);
         $this->assertSame(15000.0, $result['modal_per_unit']);
         $this->assertSame(150000.0, $result['modal_batch']);
         $this->assertSame(10000.0, $result['margin_per_unit']);
@@ -37,5 +38,20 @@ class ProductCostCalculatorTest extends TestCase
 
         $this->assertSame(1000.0, $result['modal_per_unit']);
         $this->assertNull($result['margin_persen']);
+    }
+
+    public function test_operational_rows_are_added_to_unit_cost(): void
+    {
+        $result = ProductCostCalculator::calculate(
+            [['jumlah_per_unit' => 2, 'biaya_per_unit' => 5000]],
+            0,
+            1,
+            20000,
+            [['nominal' => 3000], ['nominal' => 2000]]
+        );
+
+        $this->assertSame(10000.0, $result['modal_bahan']);
+        $this->assertSame(5000.0, $result['biaya_operasional']);
+        $this->assertSame(15000.0, $result['modal_per_unit']);
     }
 }
